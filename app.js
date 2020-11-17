e5294032175887f2507fbd7d8226c385ef1cc89e
PhilleBOI <54800654+PhilleBOI@users.noreply.github.com>
2020-11-17 10:47:22 +0100

    11-17

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const staticDir = __dirname + '\\client\\'
+
+app.use(express.static(staticDir))
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.post('/', function (req, res) {
+  res.send(req.body.message)
+})
+
+app.get('/', (req, res) => res.sendFile(__dirname + '\\client\\index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
