fc2f61c33fc9d353ba6cd858e5914af0a0ba6856
PhilleBOI <54800654+PhilleBOI@users.noreply.github.com>
2020-11-24 14:13:09 +0100

    11-24

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express')
+const personModel = require('./PersonModel')
 const app = express()
 const port = 3000
 
 const staticDir = __dirname + '\\client\\'
 
-app.use(express.static(staticDir))
-
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(express.static(staticDir))
 
-app.post('/', function (req, res) {
-  res.send(req.body.message)
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+
+  const nameList = []
+  res.render('pages/index.ejs', {name: "Niklas", nameList: nameList})
+
 })
 
-app.get('/', (req, res) => res.sendFile(__dirname + '\\client\\index.html'))
+app.post('/savePerson', function (req, res) {
+
+  let person = personModel.cratePerson(req.body.name, req.body.email, req.body.age)
+
+  dBModule.storeElement(person)
+
+  app.render('pages/index.ejs', req.body.name)
+})
+
+app.get('/', (req, res) => res.sendFile(__dirname + '\\client\\index.ejs'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
